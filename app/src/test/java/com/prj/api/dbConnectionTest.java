6a8f4523860428e2a25a708d846d4773fc55ccce
fujii0711 <fujii0711@daum.net>
2016-11-15 03:38:56 +0900

    11

--- app/src/test/java/com/prj/api/dbConnectionTest.java
+++ app/src/test/java/com/prj/api/dbConnectionTest.java
@@ -1,21 +1,21 @@
 package com.prj.api;
 import java.sql.Connection;
 
-import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"file:src/main/resources/config/database/datasource.xml"})
+@ContextConfiguration(locations={"file:src/main/resources/config/context/datasource-context.xml"})
 public class dbConnectionTest {
 
-	@Inject
+	@Autowired
 	DataSource dataSource;
 	
 	@Test
